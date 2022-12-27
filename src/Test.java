06950b9e185e08362057ef7bfa15900a65a616bc
SushilRathour <120555550+SushilRathour@users.noreply.github.com>
2022-12-27 20:54:58 +0530

    multiplication function added

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,11 @@ public class Test {
 	
 	public static void main(String args[])   
 	{  
-	int n1 = 25, n2 = 15, sum;  
+	int n1 = 25, n2 = 15, sum,mul;  
 	sum = n1 + n2;  
 	System.out.println("The sum of numbers is: "+sum);  
+	mul=n1 * n2;
+	System.out.println("The sum of numbers is: "+mul);	
 	}  
 
 }
